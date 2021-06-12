23df42b7873668a448a8c4357c41044320cdbf34
Adam <theadammiller56@gmail.com>
2021-06-12 15:01:21 -0500

    HTML + CSS and JS testing

--- WebsiteExercises/WebsiteExercises/wwwroot/js/site.js
+++ WebsiteExercises/WebsiteExercises/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function showHide(id) {
+    var element = document.getElementById(id);
+    if (element.style.display !== "none") {
+        element.style.display = "none"
+        }
+    else {
+        element.style.display ="block"
+    }
+}
